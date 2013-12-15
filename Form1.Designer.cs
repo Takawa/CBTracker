079308e0a908ffc463ed56bd546b3f9e009c6b45
Joshua Pohl <joshuapohl@gmail.com>
2013-12-14 22:22:22 -0600

    Added source files

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Coinbase_Investment_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GridInvestments = new System.Windows.Forms.DataGridView();
+            this.LabelTotalInvested = new System.Windows.Forms.Label();
+            this.LabelTotalSell = new System.Windows.Forms.Label();
+            this.TextboxBitcoinAmount = new System.Windows.Forms.TextBox();
+            this.LabelBitcoinAmount = new System.Windows.Forms.Label();
+            this.LabelDollarAmount = new System.Windows.Forms.Label();
+            this.TextboxDollarAmount = new System.Windows.Forms.TextBox();
+            this.ButtonSubmit = new System.Windows.Forms.Button();
+            this.LabelNoticeDollar = new System.Windows.Forms.Label();
+            this.LabelNoticeSellFees = new System.Windows.Forms.Label();
+            this.LabelTotalBTC = new System.Windows.Forms.Label();
+            this.QrDonate = new System.Windows.Forms.PictureBox();
+            this.LabelDonation = new System.Windows.Forms.Label();
+            this.numberHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BitHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DollarHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChangeHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SellHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GridInvestments)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QrDonate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridInvestments
+            // 
+            this.GridInvestments.AllowUserToAddRows = false;
+            this.GridInvestments.AllowUserToDeleteRows = false;
+            this.GridInvestments.AllowUserToResizeColumns = false;
+            this.GridInvestments.AllowUserToResizeRows = false;
+            this.GridInvestments.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GridInvestments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridInvestments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numberHeader,
+            this.BitHeader,
+            this.DollarHeader,
+            this.DateHeader,
+            this.ChangeHeader,
+            this.SellHeader});
+            this.GridInvestments.Location = new System.Drawing.Point(169, 12);
+            this.GridInvestments.Name = "GridInvestments";
+            this.GridInvestments.Size = new System.Drawing.Size(568, 413);
+            this.GridInvestments.TabIndex = 0;
+            this.GridInvestments.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GridInvestments_KeyDown);
+            // 
+            // LabelTotalInvested
+            // 
+            this.LabelTotalInvested.AutoSize = true;
+            this.LabelTotalInvested.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTotalInvested.Location = new System.Drawing.Point(221, 436);
+            this.LabelTotalInvested.Name = "LabelTotalInvested";
+            this.LabelTotalInvested.Size = new System.Drawing.Size(203, 39);
+            this.LabelTotalInvested.TabIndex = 1;
+            this.LabelTotalInvested.Text = "USD Invested:";
+            // 
+            // LabelTotalSell
+            // 
+            this.LabelTotalSell.AutoSize = true;
+            this.LabelTotalSell.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTotalSell.Location = new System.Drawing.Point(221, 514);
+            this.LabelTotalSell.Name = "LabelTotalSell";
+            this.LabelTotalSell.Size = new System.Drawing.Size(152, 39);
+            this.LabelTotalSell.TabIndex = 2;
+            this.LabelTotalSell.Text = "Total Sell: ";
+            // 
+            // TextboxBitcoinAmount
+            // 
+            this.TextboxBitcoinAmount.Location = new System.Drawing.Point(24, 34);
+            this.TextboxBitcoinAmount.Name = "TextboxBitcoinAmount";
+            this.TextboxBitcoinAmount.Size = new System.Drawing.Size(100, 27);
+            this.TextboxBitcoinAmount.TabIndex = 3;
+            // 
+            // LabelBitcoinAmount
+            // 
+            this.LabelBitcoinAmount.AutoSize = true;
+            this.LabelBitcoinAmount.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelBitcoinAmount.Location = new System.Drawing.Point(20, 12);
+            this.LabelBitcoinAmount.Name = "LabelBitcoinAmount";
+            this.LabelBitcoinAmount.Size = new System.Drawing.Size(94, 19);
+            this.LabelBitcoinAmount.TabIndex = 4;
+            this.LabelBitcoinAmount.Text = "BTC Amount";
+            // 
+            // LabelDollarAmount
+            // 
+            this.LabelDollarAmount.AutoSize = true;
+            this.LabelDollarAmount.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDollarAmount.Location = new System.Drawing.Point(17, 76);
+            this.LabelDollarAmount.Name = "LabelDollarAmount";
+            this.LabelDollarAmount.Size = new System.Drawing.Size(110, 19);
+            this.LabelDollarAmount.TabIndex = 5;
+            this.LabelDollarAmount.Text = "Dollar Amount";
+            // 
+            // TextboxDollarAmount
+            // 
+            this.TextboxDollarAmount.Location = new System.Drawing.Point(21, 98);
+            this.TextboxDollarAmount.Name = "TextboxDollarAmount";
+            this.TextboxDollarAmount.Size = new System.Drawing.Size(100, 27);
+            this.TextboxDollarAmount.TabIndex = 6;
+            // 
+            // ButtonSubmit
+            // 
+            this.ButtonSubmit.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSubmit.Location = new System.Drawing.Point(24, 131);
+            this.ButtonSubmit.Name = "ButtonSubmit";
+            this.ButtonSubmit.Size = new System.Drawing.Size(75, 29);
+            this.ButtonSubmit.TabIndex = 7;
+            this.ButtonSubmit.Text = "Submit";
+            this.ButtonSubmit.UseVisualStyleBackColor = true;
+            this.ButtonSubmit.Click += new System.EventHandler(this.ButtonSubmit_Click);
+            // 
+            // LabelNoticeDollar
+            // 
+            this.LabelNoticeDollar.AutoSize = true;
+            this.LabelNoticeDollar.Location = new System.Drawing.Point(17, 177);
+            this.LabelNoticeDollar.Name = "LabelNoticeDollar";
+            this.LabelNoticeDollar.Size = new System.Drawing.Size(120, 57);
+            this.LabelNoticeDollar.TabIndex = 8;
+            this.LabelNoticeDollar.Text = "*include all fees \r\nin dollar amount\r\nsubmission";
+            // 
+            // LabelNoticeSellFees
+            // 
+            this.LabelNoticeSellFees.AutoSize = true;
+            this.LabelNoticeSellFees.Location = new System.Drawing.Point(17, 245);
+            this.LabelNoticeSellFees.Name = "LabelNoticeSellFees";
+            this.LabelNoticeSellFees.Size = new System.Drawing.Size(123, 76);
+            this.LabelNoticeSellFees.TabIndex = 9;
+            this.LabelNoticeSellFees.Text = "*all sell values \r\ninclude both\r\nthe Coinbase fee \r\nand bank fee";
+            // 
+            // LabelTotalBTC
+            // 
+            this.LabelTotalBTC.AutoSize = true;
+            this.LabelTotalBTC.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTotalBTC.Location = new System.Drawing.Point(221, 475);
+            this.LabelTotalBTC.Name = "LabelTotalBTC";
+            this.LabelTotalBTC.Size = new System.Drawing.Size(185, 39);
+            this.LabelTotalBTC.TabIndex = 10;
+            this.LabelTotalBTC.Text = "BTC Owned: ";
+            // 
+            // QrDonate
+            // 
+            this.QrDonate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("QrDonate.BackgroundImage")));
+            this.QrDonate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.QrDonate.Location = new System.Drawing.Point(12, 434);
+            this.QrDonate.Name = "QrDonate";
+            this.QrDonate.Size = new System.Drawing.Size(119, 116);
+            this.QrDonate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.QrDonate.TabIndex = 11;
+            this.QrDonate.TabStop = false;
+            // 
+            // LabelDonation
+            // 
+            this.LabelDonation.AutoSize = true;
+            this.LabelDonation.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDonation.Location = new System.Drawing.Point(17, 415);
+            this.LabelDonation.Name = "LabelDonation";
+            this.LabelDonation.Size = new System.Drawing.Size(110, 19);
+            this.LabelDonation.TabIndex = 12;
+            this.LabelDonation.Text = "Donation Code";
+            // 
+            // numberHeader
+            // 
+            this.numberHeader.HeaderText = "#";
+            this.numberHeader.Name = "numberHeader";
+            this.numberHeader.ReadOnly = true;
+            this.numberHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.numberHeader.Width = 25;
+            // 
+            // BitHeader
+            // 
+            this.BitHeader.HeaderText = "BTC Amount";
+            this.BitHeader.Name = "BitHeader";
+            this.BitHeader.ReadOnly = true;
+            this.BitHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // DollarHeader
+            // 
+            this.DollarHeader.HeaderText = "USD Amount";
+            this.DollarHeader.Name = "DollarHeader";
+            this.DollarHeader.ReadOnly = true;
+            this.DollarHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // DateHeader
+            // 
+            this.DateHeader.HeaderText = "Date Logged";
+            this.DateHeader.Name = "DateHeader";
+            this.DateHeader.ReadOnly = true;
+            this.DateHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // ChangeHeader
+            // 
+            this.ChangeHeader.HeaderText = "USD Change";
+            this.ChangeHeader.Name = "ChangeHeader";
+            this.ChangeHeader.ReadOnly = true;
+            this.ChangeHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // SellHeader
+            // 
+            this.SellHeader.HeaderText = "Current Sell Value";
+            this.SellHeader.Name = "SellHeader";
+            this.SellHeader.ReadOnly = true;
+            this.SellHeader.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.LabelDonation);
+            this.Controls.Add(this.QrDonate);
+            this.Controls.Add(this.LabelTotalBTC);
+            this.Controls.Add(this.LabelNoticeSellFees);
+            this.Controls.Add(this.LabelNoticeDollar);
+            this.Controls.Add(this.ButtonSubmit);
+            this.Controls.Add(this.TextboxDollarAmount);
+            this.Controls.Add(this.LabelDollarAmount);
+            this.Controls.Add(this.LabelBitcoinAmount);
+            this.Controls.Add(this.TextboxBitcoinAmount);
+            this.Controls.Add(this.LabelTotalSell);
+            this.Controls.Add(this.LabelTotalInvested);
+            this.Controls.Add(this.GridInvestments);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Coinbase Investment Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridInvestments)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QrDonate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridInvestments;
+        private System.Windows.Forms.Label LabelTotalInvested;
+        private System.Windows.Forms.Label LabelTotalSell;
+        private System.Windows.Forms.TextBox TextboxBitcoinAmount;
+        private System.Windows.Forms.Label LabelBitcoinAmount;
+        private System.Windows.Forms.Label LabelDollarAmount;
+        private System.Windows.Forms.TextBox TextboxDollarAmount;
+        private System.Windows.Forms.Button ButtonSubmit;
+        private System.Windows.Forms.Label LabelNoticeDollar;
+        private System.Windows.Forms.Label LabelNoticeSellFees;
+        private System.Windows.Forms.Label LabelTotalBTC;
+        private System.Windows.Forms.PictureBox QrDonate;
+        private System.Windows.Forms.Label LabelDonation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numberHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BitHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DollarHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChangeHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SellHeader;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿// Author: Joshua Pohl
+// Contact: joshuapohl@gmail.com
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Coinbase_Investment_Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // date and time is retrieved for logging
+            DateTime dateAndTime = DateTime.Now;
+            p_access.completeDate = dateAndTime.ToString("d");
+
+            // the file name of the log file is stored
+            p_access.fileName = "CoinbaseInvestmentLog.txt";
+
+            // if the log file does not exist then it is created
+            if (!File.Exists(p_access.fileName))
+            {
+                var fileToClose = File.Create(p_access.fileName);
+                fileToClose.Close();
+            }
+
+            // investment information is retrieved from the log file
+            GetInvestmentsFromFile();
+            UpdateUI();
+        }
+
+        // this function reads in the information from the log file and stores it in a list
+        private void GetInvestmentsFromFile()
+        {
+            using (StreamReader sr = new StreamReader(p_access.fileName))
+            {
+                while (sr.Peek() >= 0) // while there are more characters
+                {
+                    string tmpStr;
+                    tmpStr = sr.ReadLine(); // read in a line
+
+                    string[] tmpStrArray = tmpStr.Split(new char[] { '\t' }); // split by \t into array
+
+                    // information from array is added to the list
+                    i_investment c_investment = new i_investment();
+                    Double.TryParse(tmpStrArray[0], out c_investment.bitcoinAmount);
+                    Double.TryParse(tmpStrArray[1], out c_investment.dollarAmount);
+                    c_investment.date = tmpStrArray[2];
+                    p_access.investments.Add(c_investment);
+                }
+            }
+        }
+
+        // this function updates the different aspects of the UI
+        private void UpdateUI()
+        {
+            double totalDollarInvested = 0.0;
+            double totalBtcPurchased = 0.0;
+            double totalPotentialSell = 0.0;
+
+            // clears all rows to prevent errors
+            GridInvestments.Rows.Clear();
+
+            // fills in the grid
+            // numbers are rounded to make grid easier to view
+            for (int i = 0; i < p_access.investments.Count; i++)
+            {
+                GetCoinbaseSellPrice();
+                
+                GridInvestments.Rows.Add();
+
+                GridInvestments.Rows[i].Cells[0].Value = i + 1;
+
+                GridInvestments.Rows[i].Cells[1].Value = Math.Round(p_access.investments[i].bitcoinAmount, 5);
+                totalBtcPurchased += p_access.investments[i].bitcoinAmount;
+
+                GridInvestments.Rows[i].Cells[2].Value = Math.Round(p_access.investments[i].dollarAmount, 2);
+                totalDollarInvested += p_access.investments[i].dollarAmount;
+
+                GridInvestments.Rows[i].Cells[3].Value = p_access.investments[i].date;
+
+                double change = p_access.investments[i].bitcoinAmount * (p_access.coinbaseTotalSell - p_access.coinbaseBankFee) + p_access.coinbaseBankFee
+                    - p_access.investments[i].dollarAmount;
+                GridInvestments.Rows[i].Cells[4].Value = Math.Round(change, 2);
+
+                double currentSell = Math.Round(p_access.investments[i].bitcoinAmount * (p_access.coinbaseTotalSell - p_access.coinbaseBankFee)
+                    + p_access.coinbaseBankFee, 2);
+                GridInvestments.Rows[i].Cells[5].Value = Math.Round(currentSell, 2);
+                totalPotentialSell += currentSell - p_access.coinbaseBankFee;
+            }
+
+            totalPotentialSell += p_access.coinbaseBankFee;
+
+            // labels are updated
+            LabelTotalInvested.Text = "USD Invested: $" + Math.Round(totalDollarInvested, 2);
+            LabelTotalBTC.Text = "BTC Owned: " + Math.Round(totalBtcPurchased, 5);
+            LabelTotalSell.Text = "Total Sell Value: $" + Math.Round(totalPotentialSell, 2);
+        }
+
+        // pings Coinbase and updates the selling info
+        private void GetCoinbaseSellPrice()
+        {
+            WebClient webClient = new WebClient();
+            string coinbaseString = webClient.DownloadString(p_access.coinbaseSellURL);
+            CoinbaseApiSellClass coinbaseSellInfo = JsonConvert.DeserializeObject<CoinbaseApiSellClass>(coinbaseString);
+
+            Double.TryParse(coinbaseSellInfo.subtotal.amount, out p_access.coinbaseSubtotal);
+            Double.TryParse(coinbaseSellInfo.fees[0].coinbase.amount, out p_access.coinbaseSellFee);
+            Double.TryParse(coinbaseSellInfo.fees[1].bank.amount, out p_access.coinbaseBankFee);
+            p_access.coinbaseTotalSell = p_access.coinbaseSubtotal - p_access.coinbaseSellFee - p_access.coinbaseBankFee;
+        }
+
+        // saves changed made to the list to the log file
+        private void SaveChanges()
+        {
+            using (System.IO.StreamWriter fout = new System.IO.StreamWriter(p_access.fileName))
+            {
+                for (int i = 0; i < p_access.investments.Count; i++)
+                {
+                    fout.WriteLine(p_access.investments[i].bitcoinAmount + "\t" + p_access.investments[i].dollarAmount
+                        + "\t" + p_access.investments[i].date);
+                }
+            }
+        }
+
+        // submits the information from the submission textboxes to the list, saves the changes, and updates the UI
+        private void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            double btc = 0.0;
+            double dollars = 0.0;
+            Double.TryParse(TextboxBitcoinAmount.Text, out btc);
+            Double.TryParse(TextboxDollarAmount.Text, out dollars);
+
+            // textboxes are cleared
+            TextboxBitcoinAmount.Text = "";
+            TextboxDollarAmount.Text = "";
+
+            // if the input is negative then it is made positive
+            if (btc < 0.0) btc = btc * -1.0;
+            if (dollars < 0.0) dollars = dollars * -1.0;
+
+            i_investment c_investment = new i_investment();
+            c_investment.bitcoinAmount = btc;
+            c_investment.dollarAmount = dollars;
+            c_investment.date = p_access.completeDate;
+            p_access.investments.Add(c_investment);
+
+            SaveChanges();
+            UpdateUI();
+        }
+
+        // this function will delete the row the user has selected if they press the delete key
+        private void GridInvestments_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                int rowToDelete = GridInvestments.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+                if (rowToDelete > 0)
+                {
+                    p_access.investments.RemoveAt(GridInvestments.SelectedRows[0].Index);
+
+                    GridInvestments.Rows.RemoveAt(GridInvestments.SelectedRows[0].Index);
+                }
+            }
+
+            SaveChanges();
+            UpdateUI();
+        }
+
+        // will update when called by the timer event handler
+        private void TimerTick(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+        
+        // sets up the timer and will call the TimerTick to update the UI every 15 seconds
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 15 * 1000; // 15 seconds
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        // public files that can be used with all functions
+        public static class p_access
+        {
+            public static List<i_investment> investments = new List<i_investment>();
+            public static string fileName;
+            public static string completeDate;
+
+            public static string coinbaseSellURL = "https://coinbase.com/api/v1/prices/sell";
+            public static double coinbaseBankFee = 0.0;
+            public static double coinbaseSellFee = 0.0;
+            public static double coinbaseSubtotal = 0.0;
+            public static double coinbaseTotalSell = 0.0;
+        }
+
+        // class for individual investments
+        public class i_investment
+        {
+            public double bitcoinAmount;
+            public double dollarAmount;
+            public string date;
+        }
+
+        // classes below are for the Coinbase API response
+        public class Subtotal
+        {
+            public string amount { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Coinbase
+        {
+            public string amount { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Bank
+        {
+            public string amount { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Fee
+        {
+            public Coinbase coinbase { get; set; }
+            public Bank bank { get; set; }
+        }
+
+        public class Total
+        {
+            public string amount { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class CoinbaseApiSellClass
+        {
+            public Subtotal subtotal { get; set; }
+            public List<Fee> fees { get; set; }
+            public Total total { get; set; }
+            public string amount { get; set; }
+            public string currency { get; set; }
+        }
+    }
+}
